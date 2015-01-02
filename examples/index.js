ca820f4c361895e680cee427f206d7564dd42742
Kyle Mathews <mathews.kyle@gmail.com>
2015-01-02 09:43:25 -0800

    Update examples

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,8 @@
 var SmartTimeAgo = require('../dist/index.js');
 var React = require('react');
 var moment = require('moment');
-console.log(React.renderComponentToString(SmartTimeAgo()));
+console.log(React.renderToString(SmartTimeAgo()));
 var date = new Date(1987, 4, 8, 5, 0, 0, 0);
 var recent = new moment().subtract(3, 'weeks');
 
-React.renderComponent(React.DOM.div(null, [SmartTimeAgo(), React.DOM.br(), SmartTimeAgo({value: recent}), React.DOM.br(), SmartTimeAgo({value:date})]), document.body);
+React.render(React.DOM.div(null, [SmartTimeAgo(), React.DOM.br(), SmartTimeAgo({value: recent}), React.DOM.br(), SmartTimeAgo({value:date})]), document.body);
